9997cbec78888a76b8ab72a5f8cbd1cc5617fc0f
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-03-30 08:19:11 -0600

    Modified vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/labdeweb_pets/'
-  : '/'
+  publicPath: process.env.NODE_ENV === "production" ? "/labdeweb_pets/" : "/",
   transpileDependencies: true
 })
