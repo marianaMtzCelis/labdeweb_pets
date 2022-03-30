1a0ed7b2240ebc202bea898b3fb4ff160e2de7ad
marianaMtzCelis <42619873+marianaMtzCelis@users.noreply.github.com>
2022-03-29 23:39:32 -0600

    Updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/labdeweb_pets/'
+  : '/'
   transpileDependencies: true
 })
